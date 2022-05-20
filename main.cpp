5ddb4119099f4f2dde0a84a5947c17b76e6d370b
brccabral <cabral.brc@gmail.com>
2022-05-19 21:20:52 -0700

    array of char

--- main.cpp
+++ main.cpp
@@ -3,26 +3,62 @@
 int main()
 {
 
-    int scores[]{1, 2, 5};
+    // Declare array
+    char message[6]{"Hello"}; // 5 chars, but string ends with \n
+    int data[5]{1, 2, 3, 3, 3};
 
-    int count{std::size(scores)}; // std::size( C++17)
+    std::cout << "data : " << data << std::endl; // prints garbage 0000008F3E2FF780
 
-    std::cout << "sizeof(scores) : " << sizeof(scores) << std::endl;
-    std::cout << "sizeof(scores[0]) : " << sizeof(scores[0]) << std::endl;
-    std::cout << "count : " << count << std::endl;
+    // Print out the array through looping
 
-    int count2{sizeof(scores) / sizeof(scores[0])}; // before C++17
-
-    for (size_t i{0}; i < count2; ++i)
+    std::cout << "message : ";
+    for (auto c : message)
     {
-        std::cout << "scores [" << i << "] : " << scores[i] << std::endl;
+        std::cout << c;
     }
+    std::cout << std::endl; // Hello
 
-    // Range based for loop
-    for (auto i : scores)
+    // Change characters in our array
+    message[1] = 'a';
+
+    // Print out the array through looping
+    std::cout << "message : ";
+    for (auto c : message)
     {
-        std::cout << "value  : " << i << std::endl;
+        std::cout << c;
     }
+    std::cout << std::endl; // Hallo
+
+    std::cout << "message : " << message << std::endl; // Hallo
+
+    // If a character array is null terminated, it's called as C-String
+
+    char message1[]{'H', 'e', 'l', 'l', 'o', '\0'};
+    std::cout << "message1 : " << message1 << std::endl;                 // Hello
+    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl; // 6
+
+    char message2[6]{'H', 'e', 'l', 'l', 'o'};                           // size is 6, the 6th char is null
+    std::cout << "message2 : " << message2 << std::endl;                 // Hello
+    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl; // 6
+
+    char message3[]{'H', 'e', 'l', 'l', 'o'};                            // This is not a c string ,
+                                                                         // as there is not null character
+    std::cout << "message3 : " << message3 << std::endl;                 // Hello
+    std::cout << "sizeof(message3) : " << sizeof(message3) << std::endl; // 5
+
+    // String literal
+
+    char message4[]{"Hello"};
+    std::cout << "message4 : " << message4 << std::endl;                 // Hello
+    std::cout << "sizeof(message4) : " << sizeof(message4) << std::endl; // 6
+
+    char message5[5]{'H', 'e', 'l', 'l', 'o'};
+    std::cout << "message5 : " << message5 << std::endl;                 // Hello
+    std::cout << "sizeof(message5) : " << sizeof(message5) << std::endl; // 5
+
+    // Can't safely print out arrays other than those of characters
+    int numbers[]{1, 2, 3, 4, 5};
+    std::cout << "numbers :  " << numbers << std::endl; // prints garbage 0000008F3E2FF798
 
     return 0;
 }
