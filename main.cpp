0e22fc002129437851675af4db950a40b6cfc56d
brccabral <cabral.brc@gmail.com>
2022-05-19 21:07:31 -0700

    for each with init condition

--- main.cpp
+++ main.cpp
@@ -32,5 +32,10 @@ int main()
         std::cout << " value : " << value << std::endl;
     }
 
+    for (double multiplier{4}; auto i : {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    {
+        std::cout << "result : " << (i * multiplier) << std::endl;
+    }
+
     return 0;
 }
