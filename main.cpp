d8248b0b48d58e23f220e3a981380fb48ea4afd2
brccabral <cabral.brc@gmail.com>
2022-05-20 00:11:33 -0700

    for loops

--- main.cpp
+++ main.cpp
@@ -1,64 +1,26 @@
 #include <iostream>
-#include <string>
-
-// Tools
-const int Pen{10};
-const int Marker{20};
-const int Eraser{30};
-const int Rectangle{40};
-const int Circle{50};
-const int Ellipse{60};
 
 int main()
 {
+    const size_t COUNT{100};
 
-    int tool{Eraser};
-
-    switch (tool) // can be Integral types and enums : int, long, unsigned short, etc
+    for (size_t i{0}; i < COUNT; i++)
     {
-    case Pen:
+        std::cout << i << " : I love C++" << std::endl;
+    }
+    std::cout << "Loop done!" << std::endl;
+    std::cout << "sizeof(size_t) : " << sizeof(size_t) << std::endl; // 8 bytes
+    // std::cout << "i : " << i << std::endl;                           // Compiler error : i is not in scope
+
+    size_t j{0}; // Iterator defined outside
+
+    // for (j ; j < COUNT; ++j)
+    for (; j < COUNT; ++j)
     {
-        std::cout << "Active tool is Pen" << std::endl;
+        std::cout << j << " : I love C++" << std::endl;
     }
-    break;
-
-    case Marker:
-    {
-        std::cout << "Active tool is Marker" << std::endl;
-    }
-    break;
-
-    case Eraser:
-    case Rectangle:
-    case Circle:
-    {
-        std::cout << "Drawing Shapes" << std::endl;
-    }
-    break;
-
-    case Ellipse:
-    {
-        std::cout << "Active tool is Ellipse" << std::endl;
-    }
-    break;
-
-    default:
-    {
-        std::cout << "No match found" << std::endl;
-    }
-    break;
-    }
-
-    std::cout << "Moving on" << std::endl;
-
-    /*
-    // Condition can only be integer of enum (We'll learn about enums later in the course)
-    std::string name {"John"};
-    switch (name) // Compiler error!
-    {
-
-    }
-    */
+    std::cout << "Loop done!" << std::endl;
+    std::cout << "j : " << j << std::endl;
 
     return 0;
 }
