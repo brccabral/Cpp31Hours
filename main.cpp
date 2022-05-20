b2e7db642cd3cb10e53ae2c80d0b00aa10027f32
brccabral <cabral.brc@gmail.com>
2022-05-19 17:58:06 -0700

    sum and function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+int addNumbers(int x, int y)
+{
+    return x + y;
+}
+
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    int firstnumber{3};
+    int secondnumber{7};
+    std::cout << "First " << firstnumber << std::endl;
+    std::cout << "Second " << secondnumber << std::endl;
+
+    int sum = firstnumber + secondnumber;
+    std::cout << "Sum " << sum << std::endl;
+
+    int result = addNumbers(firstnumber, 20);
+    std::cout << "Result " << result << std::endl;
+
     return 0;
 }
