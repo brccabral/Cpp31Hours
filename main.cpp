1b561ef005937a26073d2121995418dbf7b07982
brccabral <cabral.brc@gmail.com>
2022-05-19 19:39:29 -0700

    Relational and logic operations on integers

--- main.cpp
+++ main.cpp
@@ -3,28 +3,70 @@
 int main()
 {
 
-    auto var1{12};
-    auto var2{13.0};
-    auto var3{14.0f};
-    auto var4{15.0l};
-    auto var5{'e'};
+    bool a{true};
+    bool b{false};
+    bool c{true};
 
-    // int modifier suffixes
-    auto var6{123u};  // unsigned
-    auto var7{123ul}; // unsigned long
-    auto var8{123ll}; // long long
-    
-    auto var9{'efg'};
+    std::cout << std::boolalpha; // true / false
+    std::cout << "a : " << a << std::endl;
+    std::cout << "b : " << b << std::endl;
+    std::cout << "c : " << c << std::endl;
 
-    std::cout << "var1 occupies : " << sizeof(var1) << " bytes" << std::endl;
-    std::cout << "var2 occupies : " << sizeof(var2) << " bytes" << std::endl;
-    std::cout << "var3 occupies : " << sizeof(var3) << " bytes" << std::endl;
-    std::cout << "var4 occupies : " << sizeof(var4) << " bytes" << std::endl;
-    std::cout << "var5 occupies : " << sizeof(var5) << " bytes" << std::endl;
-    std::cout << "var6 occupies : " << sizeof(var6) << " bytes" << std::endl;
-    std::cout << "var7 occupies : " << sizeof(var7) << " bytes" << std::endl;
-    std::cout << "var8 occupies : " << sizeof(var8) << " bytes" << std::endl;
-    std::cout << "var9 occupies : " << sizeof(var9) << " bytes" << std::endl;
+    // AND : Evaluates to true when all operands are true.
+    //       A single false operand will drag
+    //       the entire expression to evaluating false.
+
+    std::cout << std::endl;
+    std::cout << "Basic AND operations" << std::endl;
+
+    std::cout << " a && b : " << (a && b) << std::endl;          // false
+    std::cout << " a && c : " << (a && c) << std::endl;          // true
+    std::cout << " a && b && c :" << (a && b && c) << std::endl; // false
+
+    // OR : Evaluates to true when at least one operand true.
+    //       A single true operand will push
+    //       the entire expression to evaluating true.
+    std::cout << std::endl;
+    std::cout << "Basic OR operations" << std::endl;
+    std::cout << " a || b : " << (a || b) << std::endl;
+    std::cout << " a || c : " << (a || c) << std::endl;
+    std::cout << " a ||b || c :" << (a || b || c) << std::endl;
+
+    // NOT : Negates whateve operand you put it with
+    std::cout << std::endl;
+    std::cout << "Basic NOT operations" << std::endl;
+
+    std::cout << "!a : " << !a << std::endl;
+    std::cout << "!b : " << !b << std::endl;
+    std::cout << "!c : " << !c << std::endl;
+
+    // Combinations of all these operators
+    std::cout << std::endl;
+    std::cout << "Combining logical operators" << std::endl;
+    // !(a &&b) || c
+    std::cout << "!(a &&b) || c : " << (!(a && b) || c) << std::endl; // true
+
+    // Combining logical operators with relational operators
+    int d{45};
+    int e{20};
+    int f{11};
+
+    std::cout << std::endl;
+    std::cout << "Relational and logic operations on integers" << std::endl;
+    std::cout << "d : " << d << std::endl;
+    std::cout << "e : " << e << std::endl;
+    std::cout << "f : " << f << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "(d > e) && (d > f) : " << ((d > e) && (d > f)) << std::endl; // true
+    std::cout << "(d==e) || (e <= f ) : " << ((d == e) || (e <= f)) << std::endl;
+    std::cout << "(d < e) || (d > f) : " << ((d < e) || (d > f)) << std::endl;
+    std::cout << "(f > e) || (d < f) : " << ((f > e) || (d < f)) << std::endl;
+    std::cout << "(d > f) && (f <= d) : " << ((d > f) && (f <= d)) << std::endl;
+    std::cout << "(d > e) && (d <= f) : " << ((d > e) && (d <= f)) << std::endl;
+    std::cout << "(! a) && (d == e) : " << ((!a) && (d == e)) << std::endl;
+    std::cout << "(! a) && (d == e) : " << ((!a) && (d == e)) << std::endl;
 
     return 0;
 }
