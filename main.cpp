b929f9f51ed8b8b7c10ed714810d6689adbbba58
brccabral <cabral.brc@gmail.com>
2022-05-19 21:33:49 -0700

    random numbers

--- main.cpp
+++ main.cpp
@@ -1,64 +1,40 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 int main()
 {
 
-    // Declare array
-    char message[6]{"Hello"}; // 5 chars, but string ends with \n
-    int data[5]{1, 2, 3, 3, 3};
+    std::srand(std::time(0)); // Seed
 
-    std::cout << "data : " << data << std::endl; // prints garbage 0000008F3E2FF780
+    int random_num = std::rand();
 
-    // Print out the array through looping
+    std::cout << "random_num 1: " << random_num << std::endl; // 0 ~ RAND_MAX
 
-    std::cout << "message : ";
-    for (auto c : message)
+    random_num = std::rand();
+    std::cout << "random_num 2: " << random_num << std::endl; // 0 ~ RAND_MAX
+
+    // Generate random numbers in a loop
+
+    for (size_t i{0}; i < 20; ++i)
     {
-        std::cout << c;
+        random_num = std::rand();
+        std::cout << "random_num 3: " << i << ":  " << random_num << std::endl; // 0 ~ RAND_MAX
     }
-    std::cout << std::endl; // Hello
 
-    // Change characters in our array
-    message[1] = 'a';
+    // Generate a range between 0 and 10
 
-    // Print out the array through looping
-    std::cout << "message : ";
-    for (auto c : message)
+    for (size_t i{0}; i < 20; ++i)
     {
-        std::cout << c;
+        random_num = std::rand() % 11;                                          // [ 0~10]
+        std::cout << "random_num 4: " << i << ":  " << random_num << std::endl; // 0 ~ RAND_MAX
     }
-    std::cout << std::endl; // Hallo
 
-    std::cout << "message : " << message << std::endl; // Hallo
-
-    // If a character array is null terminated, it's called as C-String
-
-    char message1[]{'H', 'e', 'l', 'l', 'o', '\0'};
-    std::cout << "message1 : " << message1 << std::endl;                 // Hello
-    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl; // 6
-
-    char message2[6]{'H', 'e', 'l', 'l', 'o'};                           // size is 6, the 6th char is null
-    std::cout << "message2 : " << message2 << std::endl;                 // Hello
-    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl; // 6
-
-    char message3[]{'H', 'e', 'l', 'l', 'o'};                            // This is not a c string ,
-                                                                         // as there is not null character
-    std::cout << "message3 : " << message3 << std::endl;                 // Hello
-    std::cout << "sizeof(message3) : " << sizeof(message3) << std::endl; // 5
-
-    // String literal
-
-    char message4[]{"Hello"};
-    std::cout << "message4 : " << message4 << std::endl;                 // Hello
-    std::cout << "sizeof(message4) : " << sizeof(message4) << std::endl; // 6
-
-    char message5[5]{'H', 'e', 'l', 'l', 'o'};
-    std::cout << "message5 : " << message5 << std::endl;                 // Hello
-    std::cout << "sizeof(message5) : " << sizeof(message5) << std::endl; // 5
-
-    // Can't safely print out arrays other than those of characters
-    int numbers[]{1, 2, 3, 4, 5};
-    std::cout << "numbers :  " << numbers << std::endl; // prints garbage 0000008F3E2FF798
+    for (size_t i{0}; i < 20; ++i)
+    {
+        random_num = std::rand() % 10 + 1;                                      // [1~10]
+        std::cout << "random_num 5: " << i << ":  " << random_num << std::endl; // 0 ~ RAND_MAX
+    }
 
     return 0;
 }
