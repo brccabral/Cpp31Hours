66b7f0f0c4735c9143e2a9506a38a7192749e583
brccabral <cabral.brc@gmail.com>
2022-05-19 19:16:18 -0700

    FractionalNumbers and precision

--- main.cpp
+++ main.cpp
@@ -1,108 +1,76 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
 
-    signed int value1{10};
-    signed int value2{-300};
+    // Declare and initialize the variables
+    float number1{1.12345678901234567890f}; // Precision : 7
+    double number2{1.12345678901234567890}; // Precision : 15
+    long double number3{1.12345678901234567890L};
 
-    std::cout << "value1 : " << value1 << std::endl;
-    std::cout << "value2 : " << value2 << std::endl;
-    std::cout << "sizeof(value1) : " << sizeof(value1) << std::endl;
-    std::cout << "sizeof(value2) : " << sizeof(value2) << std::endl;
+    // Print out the sizes
+    std::cout << "sizeof float : " << sizeof(float) << std::endl;
+    std::cout << "sizeof double : " << sizeof(double) << std::endl;
+    std::cout << "sizeof long double : " << sizeof(long double) << std::endl;
 
-    unsigned int value3{4};
-    // unsigned int value4{-5}; // Compiler error.
+    // Precision
+    std::cout << "Precision 5 ---------------" << std::endl;
+    std::cout << std::setprecision(5);                   // Control the precision from std::cout.
+    std::cout << "number1 is : " << number1 << std::endl; // 7 digits
+    std::cout << "number2 is : " << number2 << std::endl; // 15'ish digits
+    std::cout << "number3 is : " << number3 << std::endl; // 15+ digits
+    std::cout << "Precision 20 --------------" << std::endl;
+    std::cout << std::setprecision(20);                   // Control the precision from std::cout.
+    std::cout << "number1 is : " << number1 << std::endl; // 7 digits
+    std::cout << "number2 is : " << number2 << std::endl; // 15'ish digits
+    std::cout << "number3 is : " << number3 << std::endl; // 15+ digits
 
-    // short and long
-    short short_var{-32768};                 //  2 Bytes
-    short int short_int{455};                //
-    signed short signed_short{122};          //
-    signed short int signed_short_int{-456}; //
-    unsigned short int unsigned_short_int{456};
+    // Float problems : The precision is usually too limited
+    // for a lot of applications
+    float number4 = 192400023.0f; // Error : narrowing conversion
 
-    int int_var{55};           // 4 bytes
-    signed signed_var{66};     //
-    signed int signed_int{77}; //
-    unsigned int unsigned_int{77};
-    // unsigned int unsigned_neg_int{-77}; // compile error
+    std::cout << "number4 : " << number4 << std::endl;
 
-    long long_var{88}; // 4 OR 8 Bytes
-    long int long_int{33};
-    signed long signed_long{44};
-    signed long int signed_long_int{44};
-    unsigned long int unsigned_long_int{44};
+    // Scientific notation
+    // What we have seen so far in terms of floating point types
+    // is fixed notation. There is another notation, scientific
+    // that is handy if you have really huge numbers or small numbers
+    // to represent
 
-    long long long_long{888}; // 8 Bytes
-    long long int long_long_int{999};
-    signed long long signed_long_long{444};
-    signed long long int signed_long_long_int{1234};
-    unsigned long long int unsigned_long_long_int{1234};
+    std::cout << "-------------------------" << std::endl;
 
-    std::cout << "Short variable : " << short_var << " , size : "
-              << sizeof(short) << " bytes" << std::endl;
+    double number5{192400023};
+    double number6{1.92400023e8};
+    double number7{1.924e8}; // Can ommit the lower 00023
+                             // for simplicity if our application allows that.
+    double number8{0.00000000003498};
+    double number9{3.498e-11}; // multiply with 10 exp(-11)
 
-    std::cout << "Short Int : " << short_int << " , size : "
-              << sizeof(short int) << " bytes" << std::endl;
+    std::cout << "number5 is : " << number5 << std::endl;
+    std::cout << "number6 is : " << number6 << std::endl;
+    std::cout << "number7 is : " << number7 << std::endl;
+    std::cout << "number8 is : " << number8 << std::endl;
+    std::cout << "number9 is : " << number9 << std::endl;
 
-    std::cout << "Signed short : " << signed_short
-              << " , size : " << sizeof(signed short) << " bytes" << std::endl;
+    // Infinity and Nan
+    std::cout << std::endl;
+    std::cout << "Infinity and NaN" << std::endl;
 
-    std::cout << "Signed short int :  " << signed_short_int
-              << " , size : " << sizeof(signed short int) << " bytes" << std::endl;
+    double number10{-5.6};
+    double number11{}; // Initialized to 0
+    double number12{}; // Initialized to 0
 
-    std::cout << "unsigned short int :  " << unsigned_short_int
-              << " , size : " << sizeof(unsigned short int) << " bytes" << std::endl;
+    // Infinity - a number divided by 0
+    double result{number10 / number11};
 
-    std::cout << "---------------------" << std::endl;
+    std::cout << number10 << "/" << number11 << "  yields " << result << std::endl;
+    std::cout << result << " + " << number10 << " yields " << result + number10 << std::endl;
 
-    std::cout << "Int variable :  " << int_var << " , size : "
-              << sizeof(int) << " bytes" << std::endl;
+    // NaN - 0/0
+    result = number11 / number12;
 
-    std::cout << "Signed variable " << signed_var << " , size : "
-              << sizeof(signed) << " bytes" << std::endl;
-
-    std::cout << "Signed int :  " << signed_int << " , size : "
-              << sizeof(signed int) << " bytes" << std::endl;
-
-    std::cout << "unsigned int :  " << unsigned_int << " , size : "
-              << sizeof(unsigned int) << " bytes" << std::endl;
-
-    std::cout << "---------------------" << std::endl;
-
-    std::cout << "Long variable :  " << long_var << " , size : "
-              << sizeof(long) << " bytes" << std::endl;
-
-    std::cout << "Long int :  " << long_int << " , size : "
-              << sizeof(long int) << " bytes" << std::endl;
-
-    std::cout << "Signed long :  " << signed_long << " , size : "
-              << sizeof(signed long) << " bytes" << std::endl;
-
-    std::cout << "Signed long int : " << signed_long_int << " , size : "
-              << sizeof(signed long int) << " bytes" << std::endl;
-
-    std::cout << "unsigned long int : " << unsigned_long_int << " , size : "
-              << sizeof(unsigned long int) << " bytes" << std::endl;
-
-    std::cout << "---------------------" << std::endl;
-
-    std::cout << "Long long : " << long_long << " , size : "
-              << sizeof(long long) << " bytes" << std::endl;
-
-    std::cout << "Long long int : " << long_long_int << " , size : "
-              << sizeof(long long int) << " bytes" << std::endl;
-
-    std::cout << "Signed long long : " << signed_long_long << " , size : "
-              << sizeof(signed long long) << " bytes" << std::endl;
-
-    std::cout << "Signed long long int : " << signed_long_long_int << " , size : "
-              << sizeof(signed long long int) << " bytes" << std::endl;
-
-    std::cout << "unsigned long long int : " << unsigned_long_long_int << " , size : "
-              << sizeof(unsigned long long int) << " bytes" << std::endl;
-
-    std::cout << "---------------------" << std::endl;
+    std::cout << number11 << "/" << number12 << " = " << result << std::endl;
 
     return 0;
 }
