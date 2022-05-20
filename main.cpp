9a8bcbc0c9f82c4bdc6b96f62fe4e7583ad185f3
brccabral <cabral.brc@gmail.com>
2022-05-19 19:20:43 -0700

    CharactersAndText

--- main.cpp
+++ main.cpp
@@ -1,76 +1,26 @@
 #include <iostream>
-#include <iomanip>
 
 int main()
 {
 
-    // Declare and initialize the variables
-    float number1{1.12345678901234567890f}; // Precision : 7
-    double number2{1.12345678901234567890}; // Precision : 15
-    long double number3{1.12345678901234567890L};
+    char character1{'a'};
+    char character2{'r'};
+    char character3{'r'};
+    char character4{'o'};
+    char character5{'w'};
 
-    // Print out the sizes
-    std::cout << "sizeof float : " << sizeof(float) << std::endl;
-    std::cout << "sizeof double : " << sizeof(double) << std::endl;
-    std::cout << "sizeof long double : " << sizeof(long double) << std::endl;
+    std::cout << character1 << std::endl;
+    std::cout << character2 << std::endl;
+    std::cout << character3 << std::endl;
+    std::cout << character4 << std::endl;
+    std::cout << character5 << std::endl;
 
-    // Precision
-    std::cout << "Precision 5 ---------------" << std::endl;
-    std::cout << std::setprecision(5);                   // Control the precision from std::cout.
-    std::cout << "number1 is : " << number1 << std::endl; // 7 digits
-    std::cout << "number2 is : " << number2 << std::endl; // 15'ish digits
-    std::cout << "number3 is : " << number3 << std::endl; // 15+ digits
-    std::cout << "Precision 20 --------------" << std::endl;
-    std::cout << std::setprecision(20);                   // Control the precision from std::cout.
-    std::cout << "number1 is : " << number1 << std::endl; // 7 digits
-    std::cout << "number2 is : " << number2 << std::endl; // 15'ish digits
-    std::cout << "number3 is : " << number3 << std::endl; // 15+ digits
-
-    // Float problems : The precision is usually too limited
-    // for a lot of applications
-    float number4 = 192400023.0f; // Error : narrowing conversion
-
-    std::cout << "number4 : " << number4 << std::endl;
-
-    // Scientific notation
-    // What we have seen so far in terms of floating point types
-    // is fixed notation. There is another notation, scientific
-    // that is handy if you have really huge numbers or small numbers
-    // to represent
-
-    std::cout << "-------------------------" << std::endl;
-
-    double number5{192400023};
-    double number6{1.92400023e8};
-    double number7{1.924e8}; // Can ommit the lower 00023
-                             // for simplicity if our application allows that.
-    double number8{0.00000000003498};
-    double number9{3.498e-11}; // multiply with 10 exp(-11)
-
-    std::cout << "number5 is : " << number5 << std::endl;
-    std::cout << "number6 is : " << number6 << std::endl;
-    std::cout << "number7 is : " << number7 << std::endl;
-    std::cout << "number8 is : " << number8 << std::endl;
-    std::cout << "number9 is : " << number9 << std::endl;
-
-    // Infinity and Nan
+    // One byte in memory : 2^8 = 256 different values (0 ~ 255)
     std::cout << std::endl;
-    std::cout << "Infinity and NaN" << std::endl;
 
-    double number10{-5.6};
-    double number11{}; // Initialized to 0
-    double number12{}; // Initialized to 0
-
-    // Infinity - a number divided by 0
-    double result{number10 / number11};
-
-    std::cout << number10 << "/" << number11 << "  yields " << result << std::endl;
-    std::cout << result << " + " << number10 << " yields " << result + number10 << std::endl;
-
-    // NaN - 0/0
-    result = number11 / number12;
-
-    std::cout << number11 << "/" << number12 << " = " << result << std::endl;
+    char value = 65;                                                      // ASCII character code for 'A'
+    std::cout << "value : " << value << std::endl;                        // A
+    std::cout << "value(int) : " << static_cast<int>(value) << std::endl; // 65
 
     return 0;
 }
