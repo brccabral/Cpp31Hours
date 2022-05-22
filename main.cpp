278444761a79493bf340a38da3e650ef436d3526
brccabral <cabral.brc@gmail.com>
2022-05-22 04:54:47 -0700

    pointers 101

--- main.cpp
+++ main.cpp
@@ -3,39 +3,70 @@
 
 int main()
 {
-    // For 3d and really any multi dimensional array , you have to specify
-    // the number of elements in []'s , only the left most is not mandatory.
-    // Below is the example for 3D reproduced.Omitting the 5 or the 4 or both
-    // will cause a compile error.
+    // Declare and initialize pointer
+    int *p_number{}; // Will initialize with nullptr
+    double *p_fractional_number{};
 
-    // If you put less elements than the amount specified in the declaration, the compiler
-    // will automaticaly fill out those left out with 0 or '\0' is the array is of char.
+    // Explicitly initialize with nullptr
+    int *p_number1{nullptr};
+    int *p_fractional_number1{nullptr};
 
-    int house_block1[][5][4]{
+    // Pointers to different variables are of the SAME size
+    std::cout << "sizeof(int) : " << sizeof(int) << std::endl;                                   // 4
+    std::cout << "sizeof(double) : " << sizeof(double) << std::endl;                             // 8
+    std::cout << "sizeof(double*) : " << sizeof(double *) << std::endl;                          // 4
+    std::cout << "sizeof(int*) : " << sizeof(int *) << std::endl;                                // 4
+    std::cout << "sizeof(p_number1) : " << sizeof(p_number1) << std::endl;                       // 4
+    std::cout << "sizeof(p_fractional_number1) : " << sizeof(p_fractional_number1) << std::endl; // 4
 
-        {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}, {13, 14, 15}},
-        {{16, 17, 18}, {19, 20, 21}, {22, 23, 24}, {25, 26, 27}, {28, 29, 30}},
-        {{31, 32, 33}, {34, 35, 36}, {37, 38, 39}, {40, 41, 42}, {43, 44, 45}},
-        {{46, 47, 48}, {49, 50, 51}, {52, 53, 54}, {55, 56, 57}, {58, 59, 60}}
+    // It doesn't matter if you put the * close to data type or to varible name
+    int*  p_number2{nullptr};
+    int * p_number3{nullptr};
+    int  *p_number4{nullptr};
 
-    };
+    int *p_number5{}, other_int_var{};
+    int* p_number6{}, other_int_var6{}; // Confusing as you wonder if other_int_var6
+                                        // is also a pointer to int. It is not
+                                        // The structure is exactly the same for the
+                                        // previous statement
 
-    for (size_t i{0}; i < std::size(house_block1); ++i)
-    {
+    std::cout << "sizeof(p_number5) : " << sizeof(p_number5) << std::endl;
+    std::cout << "sizeof(other_int_var) : " << sizeof(other_int_var) << std::endl;
+    std::cout << "sizeof(p_number6) : " << sizeof(p_number6) << std::endl;
+    std::cout << "sizeof(other_int_var6) : " << sizeof(other_int_var6) << std::endl;
 
-        for (size_t j{0}; j < std::size(house_block1[i]); ++j)
-        {
+    // It is better to separate these declarations on different lines though
+    int *p_number7{};
+    int other_int_var7{}; // No room for confusion this time
+    
+    //Initializing pointers and assigning them data
+    //We know that pointers store addresses of variables
+    int int_var {43};
+    int *p_int{&int_var};// The address of operator (&);
+    
+    std::cout << "Int var : " << int_var << std::endl;
+    std::cout << "p_int (Address in memory)  : " << p_int << std::endl;
 
-            std::cout << "[";
-            for (size_t k{0}; k < std::size(house_block1[i][j]); ++k)
-            {
+    //You can also change the address stored in a pointer any time
+    int int_var1 {65};
+    p_int = &int_var1; // Assign a different address to the pointer
+    std::cout << "p_int (with different address)  : " << p_int << std::endl;
 
-                std::cout << house_block1[i][j][k] << " ";
-            }
-            std::cout << "] "; // Separate elements for good visualization
-        }
-        std::cout << std::endl; // Separate elements for good visualization
-    }
+
+    //Can't cross assign between pointers of different types
+    int *p_int1{nullptr};
+    double double_var{33};
+
+    //p_int = &double_var; // Compiler error
+
+
+    //Dereferencing a pointer : 
+    int* p_int2 {nullptr};
+    int int_data {56};
+    p_int2 = &int_data;
+
+    std::cout << "value : " << *p_int2 << std::endl; // Dereferencing a pointer
+
 
     return 0;
 }
