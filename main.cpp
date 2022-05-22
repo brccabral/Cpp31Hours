b47e00272e9a061f5514f114310d442c2bdb0cc2
brccabral <cabral.brc@gmail.com>
2022-05-22 10:44:41 -0700

    references and const

--- main.cpp
+++ main.cpp
@@ -3,71 +3,32 @@
 
 int main()
 {
+    // Non const reference
+    std::cout << std::endl;
+    std::cout << "Non const reference : " << std::endl;
+    int age{27};
+    int &ref_age{age};
 
-    // Declare pointer and reference
-    double double_value{12.34};
-    double &ref_double_value{double_value}; // Reference to double_value, refence requires to initiale its value, can't be null
-    double *p_double_value{&double_value};  // Pointer to double_value
+    std::cout << "age : " << age << std::endl;
+    std::cout << "ref_age : " << ref_age << std::endl;
 
-    // Reading
-    std::cout << "double_value : " << double_value << std::endl;         // 12.34
-    std::cout << "ref_double_value : " << ref_double_value << std::endl; // 12.34
-    std::cout << "p_double_value : " << p_double_value << std::endl;     // address of p_double_value
-    std::cout << "*p_double_value : " << *p_double_value << std::endl;   // 12.34
-
-    // Writting through pointer
-    *p_double_value = 15.44;
+    // Can modify original variable through reference
 
     std::cout << std::endl;
-    std::cout << "double_value : " << double_value << std::endl;         // 15.44
-    std::cout << "ref_double_value : " << ref_double_value << std::endl; // 15.44
-    std::cout << "p_double_value : " << p_double_value << std::endl;     // address of p_double_value
-    std::cout << "*p_double_value : " << *p_double_value << std::endl;   // 15.44
+    std::cout << "Modify original variable through reference : " << std::endl;
 
-    // Writting through reference
-    ref_double_value = 18.44;
+    ref_age++; // Mofify through reference
 
-    std::cout << std::endl;
-    std::cout << "double_value : " << double_value << std::endl;         // 18.44
-    std::cout << "ref_double_value : " << ref_double_value << std::endl; // 18.44
-    std::cout << "p_double_value : " << p_double_value << std::endl;     // address of p_double_value
-    std::cout << "*p_double_value : " << *p_double_value << std::endl;   // 18.44
+    std::cout << "age : " << age << std::endl;
+    std::cout << "ref_age : " << ref_age << std::endl;
 
-    double some_other_double{78.45};
+    const int &ref_age_2{age};
 
-    // Make the reference reference something else.
-    //  this is actually writing value
-    ref_double_value = some_other_double;
+    // ref_age_2++; // compile error because ref_age_2 is const
 
-    std::cout << "Making the reference reference something else..." << std::endl;
-    std::cout << std::endl;
-    std::cout << "double_value : " << double_value << std::endl;         // 78.45
-    std::cout << "ref_double_value : " << ref_double_value << std::endl; // 78.45
-    std::cout << "p_double_value : " << p_double_value << std::endl;     // address of p_double_value
-    std::cout << "*p_double_value : " << *p_double_value << std::endl;   // 78.45
-
-    // Make the pointer point to something else
-    p_double_value = &some_other_double;
-    std::cout << "Making the pointer point somewhere else..." << std::endl;
-    std::cout << std::endl;
-    std::cout << "double_value : " << double_value << std::endl;           // 78.45
-    std::cout << "ref_double_value : " << ref_double_value << std::endl;   // 78.45
-    std::cout << "&double_value : " << &double_value << std::endl;         // address of p_double_value
-    std::cout << "&ref_double_value : " << &ref_double_value << std::endl; // address of p_double_value
-    std::cout << "p_double_value : " << p_double_value << std::endl;       // changed address to some_other_double
-    std::cout << "*p_double_value : " << *p_double_value << std::endl;     // value of some_other_double 78.45
-
-    *p_double_value = 555.5;
-
-    std::cout << std::endl;
-    std::cout << "double_value : " << double_value << std::endl;             // 78.45
-    std::cout << "ref_double_value : " << ref_double_value << std::endl;     // 78.45
-    std::cout << "&double_value : " << &double_value << std::endl;           // address of p_double_value
-    std::cout << "&ref_double_value : " << &ref_double_value << std::endl;   // address of p_double_value
-    std::cout << "p_double_value : " << p_double_value << std::endl;         // changed address to some_other_double
-    std::cout << "*p_double_value : " << *p_double_value << std::endl;       // value of some_other_double 555.5
-    std::cout << "some_other_double : " << some_other_double << std::endl;   // 555.5
-    std::cout << "&some_other_double : " << &some_other_double << std::endl; // address of some_other_double
+    // Simulating reference behavior with pointers
+    const int *const p_age{&age};
+    // *p_age = 45; // compile error because age is const
 
     return 0;
 }
