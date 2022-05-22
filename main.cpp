ae5c9a95d3a11b3923910f476dc7b658efef4b8c
brccabral <cabral.brc@gmail.com>
2022-05-22 09:51:02 -0700

    null pointer safety

--- main.cpp
+++ main.cpp
@@ -2,45 +2,44 @@
 
 int main()
 {
-    /*
-    // may fail because the array is too large for memory to handle
-    //int * data = new int[10000000000000000]; //
+    // Verbose nullptr check
+    std::cout << std::endl;
+    std::cout << "Verbose nullptr check: " << std::endl;
 
-    for(size_t i{0} ; i < 10000000 ; ++i){
-        int * data = new int[100000000];
+    int *p_number{}; // Initialized to nullptr
+    // p_number = new int(7);
+
+    /*
+    if(!(p_number==nullptr)){ // just use "if (p_number)"
+        std::cout << "p_number points to a VALID address : "<< p_number << std::endl;
+        std::cout << "*p_number : " << *p_number << std::endl;
+    }else{
+        std::cout << "p_number points to an INVALID address." << std::endl;
     }
     */
-
-    // exception
-    /*
-    // will fail because we are creating too many large arrays
-     for(size_t i{0} ; i < 100 ; ++i){
-         try{
-             int * data = new int[1000000000];
-         }catch(std::exception& ex){
-             std::cout << "  Something went wrong : " << ex.what() << std::endl;
-         }
-     }
-     */
-
-    // std::nothrow
-    // this will not throw exception, but will return a null pointer that needs to be verified
-    for (size_t i{0}; i < 100; ++i)
+    if (p_number) // shor for "if(!(p_number==nullptr))"
     {
-
-        int *data = new (std::nothrow) int[1000000000];
-
-        if (data != nullptr)
-        {
-            std::cout << "Data allocated" << std::endl;
-        }
-        else
-        {
-            std::cout << "Data allocation failed" << std::endl;
-        }
+        std::cout << "p_number points to a VALID address : " << p_number << std::endl;
+        std::cout << "*p_number : " << *p_number << std::endl;
+    }
+    else
+    {
+        std::cout << "p_number points to an INVALID address." << std::endl;
     }
 
-    std::cout << "Program ending well" << std::endl;
+    delete p_number;
+    nullptr;
+
+    // It is OK call delete on a nullptr
+    // Calling delete on a nullptr is OK
+    int *p_number1{};
+
+    delete p_number1; // This won't cause any problem
+                      // if p_number1 contains nullptr
+
+    // if(p_number1 != nullptr){ // no need to check for nullptr if going to delete
+    //     delete p_number1;
+    // }
 
     return 0;
 }
