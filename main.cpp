e3e9d12571c6408cda3ce4b41f0a38949702405a
brccabral <cabral.brc@gmail.com>
2022-05-22 10:39:11 -0700

    comparing pointers and references

--- main.cpp
+++ main.cpp
@@ -4,88 +4,70 @@
 int main()
 {
 
-    int int_data{33};
-    double double_data{55};
+    // Declare pointer and reference
+    double double_value{12.34};
+    double &ref_double_value{double_value}; // Reference to double_value, refence requires to initiale its value, can't be null
+    double *p_double_value{&double_value};  // Pointer to double_value
 
-    // References
-    int &ref_int_data{int_data};
-    double &ref_double_data{double_data};
+    // Reading
+    std::cout << "double_value : " << double_value << std::endl;         // 12.34
+    std::cout << "ref_double_value : " << ref_double_value << std::endl; // 12.34
+    std::cout << "p_double_value : " << p_double_value << std::endl;     // address of p_double_value
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;   // 12.34
 
-    int tab_size = 19;
-    std::cout << std::left;
+    // Writting through pointer
+    *p_double_value = 15.44;
 
-    // Print stuff out
-    std::cout << std::setw(tab_size) << "int_data"
-              << " : " << int_data << std::endl;
-    std::cout << std::setw(tab_size) << "&int_data"
-              << " : " << &int_data << std::endl;
-    std::cout << std::setw(tab_size) << "double_data"
-              << " : " << double_data << std::endl;
-    std::cout << std::setw(tab_size) << "&double_data"
-              << " : " << &double_data << std::endl;
-
-    std::cout << "=======================" << std::endl;
-
-    std::cout << std::setw(tab_size) << "ref_int_data"
-              << " : " << ref_int_data << std::endl;
-    std::cout << std::setw(tab_size) << "&ref_int_data"
-              << " : " << &ref_int_data << std::endl;
-    std::cout << std::setw(tab_size) << "ref_double_data"
-              << " : " << ref_double_data << std::endl;
-    std::cout << std::setw(tab_size) << "&ref_double_data"
-              << " : " << &ref_double_data << std::endl;
-
-    // changing original variable
-    int_data = 111;
-    double_data = 67.2;
-
-    // Print stuff out
     std::cout << std::endl;
-    std::cout << std::setw(tab_size) << "int_data"
-              << " : " << int_data << std::endl;
-    std::cout << std::setw(tab_size) << "&int_data"
-              << " : " << &int_data << std::endl;
-    std::cout << std::setw(tab_size) << "double_data"
-              << " : " << double_data << std::endl;
-    std::cout << std::setw(tab_size) << "&double_data"
-              << " : " << &double_data << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;         // 15.44
+    std::cout << "ref_double_value : " << ref_double_value << std::endl; // 15.44
+    std::cout << "p_double_value : " << p_double_value << std::endl;     // address of p_double_value
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;   // 15.44
 
-    std::cout << "=======================" << std::endl;
+    // Writting through reference
+    ref_double_value = 18.44;
 
-    std::cout << std::setw(tab_size) << "ref_int_data"
-              << " : " << ref_int_data << std::endl;
-    std::cout << std::setw(tab_size) << "&ref_int_data"
-              << " : " << &ref_int_data << std::endl;
-    std::cout << std::setw(tab_size) << "ref_double_data"
-              << " : " << ref_double_data << std::endl;
-    std::cout << std::setw(tab_size) << "&ref_double_data"
-              << " : " << &ref_double_data << std::endl;
-
-    // changing reference variable
-    ref_int_data = 1012;
-    ref_double_data = 1000.45;
-
-    // Print stuff out
     std::cout << std::endl;
-    std::cout << std::setw(tab_size) << "int_data"
-              << " : " << int_data << std::endl;
-    std::cout << std::setw(tab_size) << "&int_data"
-              << " : " << &int_data << std::endl;
-    std::cout << std::setw(tab_size) << "double_data"
-              << " : " << double_data << std::endl;
-    std::cout << std::setw(tab_size) << "&double_data"
-              << " : " << &double_data << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;         // 18.44
+    std::cout << "ref_double_value : " << ref_double_value << std::endl; // 18.44
+    std::cout << "p_double_value : " << p_double_value << std::endl;     // address of p_double_value
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;   // 18.44
 
-    std::cout << "=======================" << std::endl;
+    double some_other_double{78.45};
 
-    std::cout << std::setw(tab_size) << "ref_int_data"
-              << " : " << ref_int_data << std::endl;
-    std::cout << std::setw(tab_size) << "&ref_int_data"
-              << " : " << &ref_int_data << std::endl;
-    std::cout << std::setw(tab_size) << "ref_double_data"
-              << " : " << ref_double_data << std::endl;
-    std::cout << std::setw(tab_size) << "&ref_double_data"
-              << " : " << &ref_double_data << std::endl;
+    // Make the reference reference something else.
+    //  this is actually writing value
+    ref_double_value = some_other_double;
+
+    std::cout << "Making the reference reference something else..." << std::endl;
+    std::cout << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;         // 78.45
+    std::cout << "ref_double_value : " << ref_double_value << std::endl; // 78.45
+    std::cout << "p_double_value : " << p_double_value << std::endl;     // address of p_double_value
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;   // 78.45
+
+    // Make the pointer point to something else
+    p_double_value = &some_other_double;
+    std::cout << "Making the pointer point somewhere else..." << std::endl;
+    std::cout << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;           // 78.45
+    std::cout << "ref_double_value : " << ref_double_value << std::endl;   // 78.45
+    std::cout << "&double_value : " << &double_value << std::endl;         // address of p_double_value
+    std::cout << "&ref_double_value : " << &ref_double_value << std::endl; // address of p_double_value
+    std::cout << "p_double_value : " << p_double_value << std::endl;       // changed address to some_other_double
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;     // value of some_other_double 78.45
+
+    *p_double_value = 555.5;
+
+    std::cout << std::endl;
+    std::cout << "double_value : " << double_value << std::endl;             // 78.45
+    std::cout << "ref_double_value : " << ref_double_value << std::endl;     // 78.45
+    std::cout << "&double_value : " << &double_value << std::endl;           // address of p_double_value
+    std::cout << "&ref_double_value : " << &ref_double_value << std::endl;   // address of p_double_value
+    std::cout << "p_double_value : " << p_double_value << std::endl;         // changed address to some_other_double
+    std::cout << "*p_double_value : " << *p_double_value << std::endl;       // value of some_other_double 555.5
+    std::cout << "some_other_double : " << some_other_double << std::endl;   // 555.5
+    std::cout << "&some_other_double : " << &some_other_double << std::endl; // address of some_other_double
 
     return 0;
 }
