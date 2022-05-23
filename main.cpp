4a2adb4dad87526df57943738b3f3411c7253256
brccabral <cabral.brc@gmail.com>
2022-05-23 11:16:58 -0700

    string compare, find char

--- main.cpp
+++ main.cpp
@@ -1,127 +1,186 @@
 #include <iostream>
+#include <cstring>
 
 int main()
 {
-    // Alpha numeric
-    std::cout << std::endl;
-    std::cout << "std::isalnum : " << std::endl;
 
-    std::cout << "C is alphanumeric : " << std::isalnum('C') << std::endl; // 1
-    std::cout << "^ is alphanumeric : " << std::isalnum('^') << std::endl; // 0
+    // std::strlen : Find the length of a string
+    //  real arrays and those decayed into pointers
+    const char message1[]{"The sky is blue."};
 
-    // Can use this as a test condition
-    char input_char{'*'};
-    if (std::isalnum(input_char))
-    {
-        std::cout << input_char << " is alhpanumeric." << std::endl;
-    }
-    else
-    {
-        std::cout << input_char << " is not alphanumeric." << std::endl;
-    }
+    // Array decays into pointer when we use const char*
+    const char *message2{"The sky is blue."};
+    std::cout << "message1 : " << message1 << std::endl;
 
-    // Alphabetic only
-    std::cout << std::endl;
-    std::cout << "std::isalpha : " << std::endl;
-    std::cout << "C is alphabetic : " << std::isalpha('e') << std::endl; // 1
-    std::cout << "^ is alphabetic : " << std::isalpha('^') << std::endl; // 0
-    std::cout << "7 is alphabetic : " << std::isalpha('7') << std::endl; // 0
+    // strlen ignores null character
+    std::cout << "strlen(message1) : " << std::strlen(message1) << std::endl; // 16
 
-    if (std::isalpha('e'))
-    {
-        std::cout << 'e' << " is alphabetic" << std::endl;
-    }
-    else
-    {
-        std::cout << 'e' << " is NOT alphabetic" << std::endl;
-    }
+    // Includes the null character
+    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl; // 17, it is an array
+
+    // strlen still works with decayed arrays
+    std::cout << "strlen(message2) : " << std::strlen(message2) << std::endl; // 16
+
+    // Prints size of pointer
+    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl; // 4, it is a pointer
+
+    // std::strcmp - signature : int strcmp( const char *lhs, const char *rhs );
+    // lhs - left hand string, rhs - right hand string
+    // Returns negative value if lhs appears before rhs in lexicographical order,
+    // Zero if lhs and rhs compare equal.
+    // and Positive value if lhs appears after rhs in lexicographical order.
+
+    // std::strcmp
+    // A comes before B = -1
+    // A equals A = 0
+    // B comes after A = 1
 
     std::cout << std::endl;
-    std::cout << "std::isblank : " << std::endl;
-    char message[]{"Hello there. How are you doing? The sun is shining."};
-    std::cout << "message : " << message << std::endl;
+    std::cout << "std::strcmp : " << std::endl;
+    const char *string_data1{"Alabama"};
+    const char *string_data2{"Blabama"};
 
-    // Find and print blank index
-    size_t blank_count{};
-    for (size_t i{0}; i < std::size(message); ++i)
-    {
-        // std::cout << "Value : " << message[i] << std::endl;
-        if (std::isblank(message[i]))
-        {
-            std::cout << "Found a blank character at index : [" << i << "]" << std::endl;
-            ++blank_count;
-        }
-    }
-    std::cout << "In total we found " << blank_count << " blank characters." << std::endl;
+    char string_data3[]{"Alabama"};
+    char string_data4[]{"Blabama"};
 
-    // Upper or Lower
-    std::cout << "std::islower and std::isupper : " << std::endl;
+    // Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
+              << std::strcmp(string_data1, string_data2) << std::endl; // -1
+
+    std::cout << "std::strcmp (" << string_data3 << "," << string_data4 << ") : "
+              << std::strcmp(string_data3, string_data4) << std::endl; // -1
+
+    string_data1 = "Alabama";
+    string_data2 = "Alabamb";
+
+    // string_data3 = "Alabama";
+    // string_data4 = "Alabamb";
+
+    // Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
+              << std::strcmp(string_data1, string_data2) << std::endl; // -1
+
+    string_data1 = "Alacama";
+    string_data2 = "Alabama";
+
+    // Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
+              << std::strcmp(string_data1, string_data2) << std::endl; // 1
+
+    string_data1 = "India";
+    string_data2 = "France";
+
+    // Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
+              << std::strcmp(string_data1, string_data2) << std::endl; // 1
+
+    string_data1 = "Kigali";
+    string_data2 = "Kigali";
+
+    // Print out the comparison
+    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
+              << std::strcmp(string_data1, string_data2) << std::endl; // 0
+
+    // std::strncmp : int strncmp( const char *lhs, const char *rhs, std::size_t count );
+    // Compares n characters in the strings
+    // Returns : Negative value if lhs appears before rhs in lexicographical order.
+    // Zero if lhs and rhs compare equal, or if count is zero.
+    // Positive value if lhs appears after rhs in lexicographical order.
+
+    // Print out the comparison
+
+    // const char *string_data1{"Alabama"};
+    // const char *string_data2{"Blabama"};
+    string_data1 = "Alabama";
+    string_data2 = "Blabama";
+    size_t n{3};
+    std::cout << std::endl;
+    std::cout << "std::strncmp : " << std::endl;
+    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : "
+              << std::strncmp(string_data1, string_data2, n) << std::endl; // -1
+
+    string_data1 = "aaaia";
+    string_data2 = "aaance";
+
+    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : "
+              << std::strncmp(string_data1, string_data2, n) << std::endl; // 0, the first 3 char are equal
+
+    n = 5;
+
+    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : "
+              << std::strncmp(string_data1, string_data2, n) << std::endl; // -1
+
+    string_data1 = "aaaoa";
+    string_data2 = "aaance";
+
+    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : "
+              << std::strncmp(string_data1, string_data2, n) << std::endl; // 1
+
+    // Find the first occurrence of a character
+    // CODE STOLEN FROM THE DOCS :
 
     std::cout << std::endl;
-    char thought[]{"The C++ Programming Language is one of the most used on the Planet"};
-    size_t lowercase_count{};
-    size_t upppercase_count{};
+    std::cout << "std::strchr : " << std::endl;
+    // doc : https://en.cppreference.com/w/cpp/string/byte/strchr
 
-    // Print original string for ease of comparison on the terminal
-    std::cout << "Original string  : " << thought << std::endl;
+    // we use std::strchr to find all the characters one by one.
 
-    for (auto character : thought)
+    // Can make this a const pointer to prevent users from making it point somewhere else
+    const char *const str{"Try not. Do, or do not. There is no try."};
+
+    char target = 'T';
+    const char *result = str;
+    size_t iterations{};
+
+    // it is comparing pointer result with target, not original str
+    while ((result = std::strchr(result, target)) != nullptr)
     {
-        if (std::islower(character))
-        {
-            std::cout << " " << character;
-            ++lowercase_count;
-        }
-        if (std::isupper(character))
-        {
-            ++upppercase_count;
-        }
+        std::cout << "Found '" << target
+                  << "' starting at '" << result << "'\n";
+
+        // Increment result, otherwise we'll find target at the same location
+        ++result; // on first iteration, result will become "ry not. Do, ......."
+        ++iterations;
     }
+    std::cout << "iterations : " << iterations << std::endl;
+
+    // A lesser version that will loop 25 times
+
+    // Find the first occurrence of a character
+    // CODE STOLEN FROM THE DOCS :
     std::cout << std::endl;
-    std::cout << "Found " << lowercase_count << " lowercase characters and "
-              << upppercase_count << " uppercase characters." << std::endl;
+    std::cout << "std::strchr : " << std::endl;
+    // doc : https://en.cppreference.com/w/cpp/string/byte/strchr
 
-    // Digits
+    // we use std::strchr to find all the characters one by one.
+
+    // str2 is not const anymore (before "const char *const str")
+    const char *str2{"Try not. Do, or do not. There is no try."};
+    result = nullptr;
+    iterations = 0;
+
+    while ((result = std::strchr(str2, target)) != nullptr)
+    {
+        std::cout << "Found '" << target
+                  << "' starting at '" << result << "'\n";
+
+        // Increment result, otherwise we'll find target at the same location
+        ++str2; // Making the pointer point to the next character
+        ++iterations;
+    }
+    std::cout << "iterations : " << iterations << std::endl;
+
+    // Find last occurence
+    // CODE STOLEN FROM THE DOCS :
     std::cout << std::endl;
-    std::cout << "std::isdigit : " << std::endl;
+    std::cout << "std::strrchr : " << std::endl;
+    // doc : https://en.cppreference.com/w/cpp/string/byte/strrchr
 
-    char statement[]{"Mr Hamilton owns 221 cows. That's a lot of cows! The kid exclamed."};
-    std::cout << "statement : " << statement << std::endl;
-
-    size_t digit_count{};
-
-    for (auto character : statement)
-    {
-        if (std::isdigit(character))
-        {
-            std::cout << "Found digit : " << character << std::endl;
-            ++digit_count;
-        }
-    }
-    std::cout << "Found " << digit_count << " digits in the statement string" << std::endl;
-
-    // Turning a character to lowercase using the std::tolower() function
-    std::cout << std::endl;
-    std::cout << "std::tolwer and std::toupper: " << std::endl;
-    char original_str[]{"Home. The feeling of belonging"};
-    char dest_str[std::size(original_str)];
-
-    // Turn this to uppercase. Change the array in place
-    for (size_t i{}; i < std::size(original_str); ++i)
-    {
-        dest_str[i] = std::toupper(original_str[i]);
-    }
-
-    std::cout << "Original string : " << original_str << std::endl;
-    std::cout << "Uppercase string : " << dest_str << std::endl;
-
-    // Turn this to lowercase. Change the array in place
-    for (size_t i{}; i < std::size(original_str); ++i)
-    {
-        dest_str[i] = std::tolower(original_str[i]);
-    }
-
-    std::cout << "Lowercase string : " << dest_str << std::endl;
+    char input[] = "/home/user/hello.cpp";
+    char *output = std::strrchr(input, '/');
+    if (output)
+        std::cout << output + 1 << std::endl; //+1 because we want to start printing past
+                                              // the character found by std::strrchr.
 
     return 0;
 }
