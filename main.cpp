16d239ba5df434334525b9632ed45e47dc88b277
brccabral <cabral.brc@gmail.com>
2022-05-25 13:30:57 -0700

    use reference to get values from functions

--- main.cpp
+++ main.cpp
@@ -1,20 +1,64 @@
 #include <iostream>
+#include <string>
 
-void say_age(int &age); // Declaration
+void max_str(const std::string &input1, const std::string input2,
+             std::string &output)
+{
+    if (input1 > input2)
+    {
+        output = input1;
+    }
+    else
+    {
+        output = input2;
+    }
+}
+
+void max_int(int input1, int input2, int &output)
+{
+    if (input1 > input2)
+    {
+        output = input1;
+    }
+    else
+    {
+        output = input2;
+    }
+}
+
+void max_double(double input1, double input2, double *output)
+{
+    if (input1 > input2)
+    {
+        *output = input1;
+    }
+    else
+    {
+        *output = input2;
+    }
+}
 
 int main()
 {
 
-    int age{23};                                                                  // Local
-    std::cout << "age (before call) : " << age << "&age : " << &age << std::endl; // 23 0x475c7ffc48
-    say_age(age);                                                                 // Argument
-    std::cout << "age (after call) : " << age << "&age : " << &age << std::endl;  // 24 0x475c7ffc48
+    std::string out_str;
+    std::string string1("Cassablanca");
+    std::string string2("Bellevue");
+
+    max_str(string1, string2, out_str);
+    std::cout << "max_str : " << out_str << std::endl; // Cassablanca
+
+    int out_int;
+    int in1{45};
+    int in2{723};
+    max_int(in1, in2, out_int);
+    std::cout << "max_int : " << out_int << std::endl; // 723
+
+    double out_double;
+    double in_double1{45.8};
+    double in_double2{76.9};
+    max_double(in_double1, in_double2, &out_double);
+    std::cout << "max_double : " << out_double << std::endl; // 76.9
 
     return 0;
-}
-
-void say_age(int &age)
-{ // Parameter
-    ++age;
-    std::cout << "Hello , you are " << age << " years old! &age : " << &age << std::endl; // 24 0x475c7ffc48 - same address
 }
