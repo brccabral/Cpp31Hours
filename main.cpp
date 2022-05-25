7e0112f4f0caeaa98780ebf4d23886e6f5e5562e
brccabral <cabral.brc@gmail.com>
2022-05-25 12:45:01 -0700

    pass by pointer

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
-void say_age(int age)
-{ // Parameter
-    ++age;
-    std::cout << "Hello , you are " << age << " years old! &age : " << &age << std::endl; // difference address
-}
+void say_age(int *age); // Declaration
 
 int main()
 {
 
     int age{23};                                                                  // Local
-    std::cout << "age (before call) : " << age << "&age : " << &age << std::endl; // 23 010FF9CC
-    say_age(age);                                                                 // Argument - difference address - 24 010FF9A8
-    std::cout << "age (after call) : " << age << "&age : " << &age << std::endl;  // 23 010FF9CC
+    std::cout << "age (before call) : " << age << "&age : " << &age << std::endl; // 23 00EFF824
+    say_age(&age);                                                                // Argument - pass the address
+    std::cout << "age (after call) : " << age << "&age : " << &age << std::endl;  // 24 same address 00EFF824
 
     return 0;
+}
+
+void say_age(int *age)
+{ // Parameter
+    ++(*age);
+    std::cout << "Hello , you are " << *age << " years old! &(*age) : " << &(*age) << " &age : " << &age << std::endl; // 24 - &(*age) original addresss 00EFF824, &age pointer address 00EFF800
 }
