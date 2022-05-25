6daccd861cd848ab42657c0ea23a09e6c0dcb210
brccabral <cabral.brc@gmail.com>
2022-05-25 13:43:01 -0700

    return keyword

--- main.cpp
+++ main.cpp
@@ -1,64 +1,38 @@
 #include <iostream>
-#include <string>
 
-void max_str(const std::string &input1, const std::string input2,
-             std::string &output)
+int sum(int a, int b)
 {
-    if (input1 > input2)
-    {
-        output = input1;
-    }
-    else
-    {
-        output = input2;
-    }
+    int result = a + b;
+    std::cout << "In : &result(int) :  " << &result << std::endl; // 0x6b0d7ff92c
+    return result;
 }
 
-void max_int(int input1, int input2, int &output)
+std::string add_strings(std::string str1, std::string str2)
 {
-    if (input1 > input2)
-    {
-        output = input1;
-    }
-    else
-    {
-        output = input2;
-    }
-}
-
-void max_double(double input1, double input2, double *output)
-{
-    if (input1 > input2)
-    {
-        *output = input1;
-    }
-    else
-    {
-        *output = input2;
-    }
+    std::cout << "Param : &str1 :  " << &str1 << std::endl; // 0xf2dc3ff5e0
+    std::cout << "Param : &str2 :  " << &str2 << std::endl; // 0xf2dc3ff5c0
+    std::string result = str1 + str2;
+    std::cout << "In : &result(std::string) :  " << &result << std::endl; // 0x6b0d7ff9e0
+    return result;
 }
 
 int main()
 {
 
-    std::string out_str;
-    std::string string1("Cassablanca");
-    std::string string2("Bellevue");
+    int x{15};
+    int y{9};
+    int result = sum(x, y);
+    std::cout << "Out : &result(int) :  " << &result << std::endl; // 0x6b0d7ff92c - different address from function
+    std::cout << "result : " << result << std::endl;               // 24
 
-    max_str(string1, string2, out_str);
-    std::cout << "max_str : " << out_str << std::endl; // Cassablanca
-
-    int out_int;
-    int in1{45};
-    int in2{723};
-    max_int(in1, in2, out_int);
-    std::cout << "max_int : " << out_int << std::endl; // 723
-
-    double out_double;
-    double in_double1{45.8};
-    double in_double2{76.9};
-    max_double(in_double1, in_double2, &out_double);
-    std::cout << "max_double : " << out_double << std::endl; // 76.9
+    std::string in_str1{"Hello"};
+    std::string in_str2{" World!"};
+    std::cout << "Main : &in_str1 :  " << &in_str1 << std::endl; // 0xf2dc3ff660 - different address from function
+    std::cout << "Main : &in_str2 :  " << &in_str2 << std::endl; // 0xf2dc3ff628 - different address from function
+    std::string result_str = add_strings(in_str1, in_str2);
+    std::cout << "Out : &result_str(std::string) :  " << &result_str << std::endl; // 0x6b0d7ff9e0 - same address from function,
+                                                                                   // the compiler did this optimization
+    std::cout << "result_str : " << result_str << std::endl;
 
     return 0;
 }
