939c1ba037b4446f2c2cdd959d5a36ab74fbd7bb
brccabral <cabral.brc@gmail.com>
2022-05-25 11:36:57 -0700

    str concat and copy

--- main.cpp
+++ main.cpp
@@ -4,183 +4,89 @@
 int main()
 {
 
-    // std::strlen : Find the length of a string
-    //  real arrays and those decayed into pointers
-    const char message1[]{"The sky is blue."};
-
-    // Array decays into pointer when we use const char*
-    const char *message2{"The sky is blue."};
-    std::cout << "message1 : " << message1 << std::endl;
-
-    // strlen ignores null character
-    std::cout << "strlen(message1) : " << std::strlen(message1) << std::endl; // 16
-
-    // Includes the null character
-    std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl; // 17, it is an array
-
-    // strlen still works with decayed arrays
-    std::cout << "strlen(message2) : " << std::strlen(message2) << std::endl; // 16
-
-    // Prints size of pointer
-    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl; // 4, it is a pointer
-
-    // std::strcmp - signature : int strcmp( const char *lhs, const char *rhs );
-    // lhs - left hand string, rhs - right hand string
-    // Returns negative value if lhs appears before rhs in lexicographical order,
-    // Zero if lhs and rhs compare equal.
-    // and Positive value if lhs appears after rhs in lexicographical order.
-
-    // std::strcmp
-    // A comes before B = -1
-    // A equals A = 0
-    // B comes after A = 1
+    // Concatenation
 
     std::cout << std::endl;
-    std::cout << "std::strcmp : " << std::endl;
-    const char *string_data1{"Alabama"};
-    const char *string_data2{"Blabama"};
+    std::cout << "std::strcat : " << std::endl;
+    // doc : https://en.cppreference.com/w/cpp/string/byte/strcat
 
-    char string_data3[]{"Alabama"};
-    char string_data4[]{"Blabama"};
+    char dest[50] = "Hello ";
+    char src[50] = "World!";
+    std::strcat(dest, src);                      // Some compilers (MSVC) think these functions are unsafe!
+    std::cout << "dest : " << dest << std::endl; // Hello World
+    std::strcat(dest, " Goodbye World!");
+    std::cout << "dest : " << dest << std::endl; // Hello World! Goodbye World!
 
-    // Print out the comparison
-    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
-              << std::strcmp(string_data1, string_data2) << std::endl; // -1
-
-    std::cout << "std::strcmp (" << string_data3 << "," << string_data4 << ") : "
-              << std::strcmp(string_data3, string_data4) << std::endl; // -1
-
-    string_data1 = "Alabama";
-    string_data2 = "Alabamb";
-
-    // string_data3 = "Alabama";
-    // string_data4 = "Alabamb";
-
-    // Print out the comparison
-    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
-              << std::strcmp(string_data1, string_data2) << std::endl; // -1
-
-    string_data1 = "Alacama";
-    string_data2 = "Alabama";
-
-    // Print out the comparison
-    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
-              << std::strcmp(string_data1, string_data2) << std::endl; // 1
-
-    string_data1 = "India";
-    string_data2 = "France";
-
-    // Print out the comparison
-    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
-              << std::strcmp(string_data1, string_data2) << std::endl; // 1
-
-    string_data1 = "Kigali";
-    string_data2 = "Kigali";
-
-    // Print out the comparison
-    std::cout << "std::strcmp (" << string_data1 << "," << string_data2 << ") : "
-              << std::strcmp(string_data1, string_data2) << std::endl; // 0
-
-    // std::strncmp : int strncmp( const char *lhs, const char *rhs, std::size_t count );
-    // Compares n characters in the strings
-    // Returns : Negative value if lhs appears before rhs in lexicographical order.
-    // Zero if lhs and rhs compare equal, or if count is zero.
-    // Positive value if lhs appears after rhs in lexicographical order.
-
-    // Print out the comparison
-
-    // const char *string_data1{"Alabama"};
-    // const char *string_data2{"Blabama"};
-    string_data1 = "Alabama";
-    string_data2 = "Blabama";
-    size_t n{3};
-    std::cout << std::endl;
-    std::cout << "std::strncmp : " << std::endl;
-    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : "
-              << std::strncmp(string_data1, string_data2, n) << std::endl; // -1
-
-    string_data1 = "aaaia";
-    string_data2 = "aaance";
-
-    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : "
-              << std::strncmp(string_data1, string_data2, n) << std::endl; // 0, the first 3 char are equal
-
-    n = 5;
-
-    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : "
-              << std::strncmp(string_data1, string_data2, n) << std::endl; // -1
-
-    string_data1 = "aaaoa";
-    string_data2 = "aaance";
-
-    std::cout << "std::strncmp (" << string_data1 << "," << string_data2 << "," << n << ") : "
-              << std::strncmp(string_data1, string_data2, n) << std::endl; // 1
-
-    // Find the first occurrence of a character
-    // CODE STOLEN FROM THE DOCS :
+    // More concatenation
 
     std::cout << std::endl;
-    std::cout << "std::strchr : " << std::endl;
-    // doc : https://en.cppreference.com/w/cpp/string/byte/strchr
+    std::cout << "More std::strcat : " << std::endl;
 
-    // we use std::strchr to find all the characters one by one.
+    char *dest1 = new char[30]{'F', 'i', 'r', 'e', 'l', 'o', 'r', 'd', '\0'};
+    char *source1 =
+        new char[30]{',', 'T', 'h', 'e', ' ', 'P', 'h', 'e', 'n', 'i', 'x', ' ', 'K', 'i', 'n', 'g', '!', '\0'};
 
-    // Can make this a const pointer to prevent users from making it point somewhere else
-    const char *const str{"Try not. Do, or do not. There is no try."};
+    // NOTE TO SELF :
+    // Describe what's going to happen in std::strcat before you do the cat :
+    // source is going to be appended to dest, and the process
+    // will start by overriding the null character in dest
 
-    char target = 'T';
-    const char *result = str;
-    size_t iterations{};
+    std::cout << "std::strlen(dest1) :" << std::strlen(dest1) << std::endl;      // 8
+    std::cout << "std::strlen(source1) : " << std::strlen(source1) << std::endl; // 17
 
-    // it is comparing pointer result with target, not original str
-    while ((result = std::strchr(result, target)) != nullptr)
-    {
-        std::cout << "Found '" << target
-                  << "' starting at '" << result << "'\n";
+    std::cout << "Concatenating..." << std::endl;
+    std::strcat(dest1, source1);
 
-        // Increment result, otherwise we'll find target at the same location
-        ++result; // on first iteration, result will become "ry not. Do, ......."
-        ++iterations;
-    }
-    std::cout << "iterations : " << iterations << std::endl;
+    std::cout << "std::strlen(dest1) :" << std::strlen(dest1) << std::endl; // 25
+    std::cout << "dest1 : " << dest1 << std::endl;                          // Firelord,The Phenix King!
 
-    // A lesser version that will loop 25 times
+    // std::strncat  : concatenates n characters from src to dest and
+    // returns a pointer to the result string
+    // signature : char *strncat( char *dest, const char *src, std::size_t count );
 
-    // Find the first occurrence of a character
-    // CODE STOLEN FROM THE DOCS :
     std::cout << std::endl;
-    std::cout << "std::strchr : " << std::endl;
-    // doc : https://en.cppreference.com/w/cpp/string/byte/strchr
+    std::cout << "std::strncat:" << std::endl;
+    char dest2[50]{"Hello"};
 
-    // we use std::strchr to find all the characters one by one.
+    char source2[30] = {" There is a bird on my window"};
 
-    // str2 is not const anymore (before "const char *const str")
-    const char *str2{"Try not. Do, or do not. There is no try."};
-    result = nullptr;
-    iterations = 0;
+    // You can even use the returned pointer immediately for print out
+    // This is a pattern you'll see a lot in C++ code out there.
+    std::cout << std::strncat(dest2, source2, 6) << std::endl; // Hello There
 
-    while ((result = std::strchr(str2, target)) != nullptr)
-    {
-        std::cout << "Found '" << target
-                  << "' starting at '" << result << "'\n";
+    // Or you can do std::strncat separately and print dest2
+    std::strncat(dest2, source2, 6);
+    std::cout << "The concatenated string is : " << dest2 << std::endl; // Hello There There
 
-        // Increment result, otherwise we'll find target at the same location
-        ++str2; // Making the pointer point to the next character
-        ++iterations;
-    }
-    std::cout << "iterations : " << iterations << std::endl;
+    // std::strcpy - signature : char* strcpy( char* dest, const char* src );
 
-    // Find last occurence
-    // CODE STOLEN FROM THE DOCS :
     std::cout << std::endl;
-    std::cout << "std::strrchr : " << std::endl;
-    // doc : https://en.cppreference.com/w/cpp/string/byte/strrchr
+    std::cout << "std::strcpy : " << std::endl;
+    const char *source3 = "C++ is a multipurpose programming language.";
+    char *dest3 = new char[std::strlen(source3) + 1]; // +1 for the null character
+                                                      // Contains garbage data
+                                                      // dest3 Not initialized
 
-    char input[] = "/home/user/hello.cpp";
-    char *output = std::strrchr(input, '/');
-    if (output)
-        std::cout << output + 1 << std::endl; //+1 because we want to start printing past
-                                              // the character found by std::strrchr.
+    std::strcpy(dest3, source3);
+
+    std::cout << "sizeof(dest3) : " << sizeof(dest3) << std::endl;           // 4 - it is a pointer
+    std::cout << "std::strlen(dest3) : " << std::strlen(dest3) << std::endl; // 43
+    std::cout << "dest3 : " << dest3 << std::endl;                           // C++ is a multipurpose programming language.
+
+    // std::strncpy : Copy n characters from src to dest -
+    // signature : char *strncpy( char *dest, const char *src, std::size_t count );
+    std::cout << std::endl;
+    std::cout << "std::strncpy:" << std::endl;
+    const char *source4 = "Hello";
+    char dest4[] = {'a', 'b', 'c', 'd', 'e', 'f', '\0'}; // Have to put the terminating
+                                                         // null char if we want to print
+
+    std::cout << "dest4 : " << dest4 << std::endl; // abcdef
+
+    std::cout << "Copying..." << std::endl;
+    std::strncpy(dest4, source4, 5);
+
+    std::cout << "dest4 : " << dest4 << std::endl; // Hellof
 
     return 0;
 }
