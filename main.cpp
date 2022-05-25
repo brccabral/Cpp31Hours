e49d517cf2a4f7baee70efcb322d64c3825c1cef
brccabral <cabral.brc@gmail.com>
2022-05-25 12:40:23 -0700

    pass by value

--- main.cpp
+++ main.cpp
@@ -1,58 +1,18 @@
 #include <iostream>
-#include "person.h"
 
-// Variable : Declaration and definition
-double weight{};
-// double weight{}; // compile error = error C2374: 'weight': redefinition; multiple initialization
-
-double add(double a, double b);
-
-struct Point
-{
-    double m_x{};
-    double m_y{};
-};
-
-// error C2011: 'Point': 'struct' type redefinition
-// it is NOT allowed because it is in same translation unit
-//  struct Point
-//  {
-//      double m_x{};
-//      double m_y{};
-//  };
-
-// linker error = fatal error LNK1169: one or more multiply defined symbols found
-// it is already defined in some_other_file.cpp
-// int Person::person_count = 8;
-// Person::Person(const std::string &names_param, int age_param)
-//     : full_name{names_param}, age{age_param}
-// {
-//     ++person_count;
-// }
+void say_age(int age)
+{ // Parameter
+    ++age;
+    std::cout << "Hello , you are " << age << " years old! &age : " << &age << std::endl; // difference address
+}
 
 int main()
 {
-    std::cout << weight << std::endl; // 0
 
-    double result = add(10, 20);
-    std::cout << result << std::endl; // 30
-
-    Point p1;
-    std::cout << "p1.m_x : " << p1.m_x << std::endl; // p1.m_x : 0
-
-    Person p2("John Snow", 35);
-    p2.print_info(); // name : John Snow , age : 35
+    int age{23};                                                                  // Local
+    std::cout << "age (before call) : " << age << "&age : " << &age << std::endl; // 23 010FF9CC
+    say_age(age);                                                                 // Argument - difference address - 24 010FF9A8
+    std::cout << "age (after call) : " << age << "&age : " << &age << std::endl;  // 23 010FF9CC
 
     return 0;
 }
-
-double add(double a, double b)
-{
-    return a + b;
-}
-
-// compile error = error C2084: function 'double add(double,double)' already has a body
-// double add(double a, double b)
-// {
-//     return a + b;
-// }
