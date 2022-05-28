b250b389968c7a56b3d7cbf68e7b0fae7d74f830
brccabral <cabral.brc@gmail.com>
2022-05-27 17:13:43 -0700

    function overload with different parameters

--- main.cpp
+++ main.cpp
@@ -1,38 +1,53 @@
 #include <iostream>
 
-int sum(int a, int b)
+int max(int a, int b)
 {
-    int result = a + b;
-    std::cout << "In : &result(int) :  " << &result << std::endl; // 0x6b0d7ff92c
-    return result;
+    std::cout << "int overload called" << std::endl;
+    return (a > b) ? a : b;
 }
 
-std::string add_strings(std::string str1, std::string str2)
+double max(double a, double b)
 {
-    std::cout << "Param : &str1 :  " << &str1 << std::endl; // 0xf2dc3ff5e0
-    std::cout << "Param : &str2 :  " << &str2 << std::endl; // 0xf2dc3ff5c0
-    std::string result = str1 + str2;
-    std::cout << "In : &result(std::string) :  " << &result << std::endl; // 0x6b0d7ff9e0
-    return result;
+    std::cout << "double overload called" << std::endl;
+    return (a > b) ? a : b;
+}
+
+double max(int a, double b)
+{
+    std::cout << "(int,double) overload called" << std::endl;
+    return (a > b) ? a : b;
+}
+
+double max(double a, int b)
+{
+    std::cout << "(double,int) overload called" << std::endl;
+    return (a > b) ? a : b;
+}
+
+double max(double a, int b, int c)
+{
+    std::cout << "(double,int,int) overload called" << std::endl;
+    return (a > b) ? ((a > c) ? a : c) : ((c > b) ? c : b);
+}
+
+std::string_view max(std::string_view a, std::string_view b)
+{
+    std::cout << "(string_view,string_view) overload called" << std::endl;
+    return (a > b) ? a : b;
 }
 
 int main()
 {
 
-    int x{15};
+    int x{4};
     int y{9};
-    int result = sum(x, y);
-    std::cout << "Out : &result(int) :  " << &result << std::endl; // 0x6b0d7ff92c - different address from function
-    std::cout << "result : " << result << std::endl;               // 24
+    double a{5.4};
+    double b{7.4};
 
-    std::string in_str1{"Hello"};
-    std::string in_str2{" World!"};
-    std::cout << "Main : &in_str1 :  " << &in_str1 << std::endl; // 0xf2dc3ff660 - different address from function
-    std::cout << "Main : &in_str2 :  " << &in_str2 << std::endl; // 0xf2dc3ff628 - different address from function
-    std::string result_str = add_strings(in_str1, in_str2);
-    std::cout << "Out : &result_str(std::string) :  " << &result_str << std::endl; // 0x6b0d7ff9e0 - same address from function,
-                                                                                   // the compiler did this optimization
-    std::cout << "result_str : " << result_str << std::endl;
+    auto result = max(b, x, y);
+    std::cout << "result " << result << std::endl;
+
+    max("Hello", "World");
 
     return 0;
 }
