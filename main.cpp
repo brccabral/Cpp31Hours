487f7a5ae016e1c8c43c40c372b54daaaff1222e
brccabral <cabral.brc@gmail.com>
2022-05-29 12:07:29 -0700

    multiples concepts

--- main.cpp
+++ main.cpp
@@ -4,59 +4,34 @@
 template <typename T>
 concept TinyType = requires(T t)
 {
-    sizeof(T) <= 4;          // Simple requirement : Only enforces syntax
-    requires sizeof(T) <= 4; // Nested requirements
+    sizeof(T) <= 4;          // Simple requirement
+    requires sizeof(T) <= 4; // Nested requirement
 };
 
-// Compound requirement
 template <typename T>
-concept Addable = requires(T a, T b)
+// requires std::integral<T> || std::floating_point<T> // OR operator
+// requires std::integral<T> && TinyType<T>
+requires std::integral<T> && requires(T t)
 {
-    // Checks if a + b is valid syntax, doesn't throw expetions(optional) , and the result
-    // is convertible to int(optional)
-    {
-        a + b
-    }
-    noexcept->std::convertible_to<int>; // Compound requirement
-
-    // noexcept is optional
-    // {a + b} -> std::convertible_to<int>; // Compound requirement
-};
-
-TinyType auto add1(TinyType auto a, TinyType auto b)
-{
-    return a + b;
+    sizeof(T) <= 4;          // Simple requirement
+    requires sizeof(T) <= 4; // Nested requirement
 }
-
-Addable auto add2(Addable auto a, Addable auto b)
+T add(T a, T b)
 {
     return a + b;
 }
 
 int main()
 {
-    char c1{71};
-    char c2{23};
 
-    auto cr = add1(c1, c2); // char is TinyType, will convert to int (size 4) because of math operation
-    std::cout << "cr : " << cr << std::endl;
-    auto cr2 = add2(c1, c2); // char is Addable
-    std::cout << "cr2 : " << cr2 << std::endl;
+    long long int x{7};
+    long long int y{5};
 
-    double x{67};
-    double y{56};
-    // auto s2 = add1(x1, y1); // string are not TinyType
+    // add(x, y); // error, "long long int" is not TinyType
 
-    // auto result = add1(x, y); // error, double is not TinyType
-    auto result = add2(x, y);
-    std::cout << "result : " << result << std::endl;
-    std::cout << "sizeof(result) : " << sizeof(result) << std::endl;
-
-    std::string x1{"Hello"};
-    std::string y1{"World"};
-
-    auto s = x1 + y1;
-    std::cout << "s : " << s << std::endl;
+    int x1{2};
+    int x2{200};
+    add(x1, x2);
 
     return 0;
 }
