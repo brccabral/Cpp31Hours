8ceb3261a67778cb41972faa4381599cf9e58afc
brccabral <cabral.brc@gmail.com>
2022-05-30 14:22:10 -0700

    destructor order

--- main.cpp
+++ main.cpp
@@ -8,57 +8,33 @@ public:
     Dog(std::string_view name_param, std::string_view breed_param, int age_param);
     ~Dog();
 
-public:
+private:
     std::string name;
     std::string breed;
+    int *p_age{nullptr};
 };
 Dog::Dog(std::string_view name_param, std::string_view breed_param, int age_param)
 {
     name = name_param;
     breed = breed_param;
+    p_age = new int;
+    *p_age = age_param;
     std::cout << "Dog constructor called for " << name << std::endl;
 }
 
 Dog::~Dog()
 {
+    delete p_age;
     std::cout << "Dog destructor called for : " << name << std::endl;
-}
-
-void some_func(Dog dog)
-{
-    // when passed by value, the constructor not called, but
-    // the destructor is called at the end of the function
-    // be careful if the object has pointers, as the pointer will be
-    // destroyed in this function and in main, causing a crash
-    dog.name = "Dragon";
-    std::cout << "Dog name : " << dog.name << std::endl;
-}
-
-void heap_func()
-{
-    Dog *p_dog = new Dog("HeapDog", "Shepherd", 2); // Heap allocation
-                                                    // need to be deleted in this local space or will cause memory leak
-
-    delete p_dog; // Causes for the destructor of Dog to be called
 }
 
 int main()
 {
 
-    Dog p_dog("Fluffy", "Shepherd", 2);
-    some_func(p_dog);
-    heap_func();
+    Dog dog1("Dogyy1", "Shepherd", 2);
+    Dog dog2("Dogyy2", "Shepherd", 3);
+    Dog dog3("Dogyy3", "Shepherd", 5);
+    Dog dog4("Dogyy4", "Shepherd", 1); // last object is destroyed first
 
-    std::cout << "Done!" << std::endl;
     return 0;
-
-    /*
-Dog constructor called for Fluffy
-Dog name : Dragon
-Dog destructor called for : Dragon
-Dog constructor called for HeapDog
-Dog destructor called for : HeapDog
-Done!
-Dog destructor called for : Fluffy
-    */
 }
