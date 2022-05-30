5bb3c467a45f88efa35c1d590f41affd7de0532b
brccabral <cabral.brc@gmail.com>
2022-05-30 14:27:13 -0700

    this pointer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string_view>
 
 class Dog
 {
@@ -7,6 +6,53 @@ public:
     Dog() = default;
     Dog(std::string_view name_param, std::string_view breed_param, int age_param);
     ~Dog();
+
+    void print_info()
+    {
+        std::cout << "Dog (" << this << ") : [ name : " << name
+                  << " breed : " << breed << " age : " << *p_age << "]" << std::endl;
+    }
+
+    // Setters
+    // Chained calls using pointers
+    /*
+    Dog *set_dog_name(std::string_view name)
+    {
+        // name = name; // This does nothing
+        this->name = name;
+        return this; // this is a pointer for this object
+    }
+    Dog *set_dog_breed(std::string_view breed)
+    {
+        this->breed = breed;
+        return this;
+    }
+
+    Dog *set_dog_age(int age)
+    {
+        *(this->p_age) = age; // p_age is pointer
+        return this;
+    }
+    */
+
+    // Chained calls using references
+    Dog &set_dog_name(std::string_view name)
+    {
+        // name = name; // This does nothing
+        this->name = name;
+        return *this;
+    }
+    Dog &set_dog_breed(std::string_view breed)
+    {
+        this->breed = breed;
+        return *this;
+    }
+
+    Dog &set_dog_age(int age)
+    {
+        *(this->p_age) = age; // p_age is pointer
+        return *this;
+    }
 
 private:
     std::string name;
@@ -19,22 +65,36 @@ Dog::Dog(std::string_view name_param, std::string_view breed_param, int age_para
     breed = breed_param;
     p_age = new int;
     *p_age = age_param;
-    std::cout << "Dog constructor called for " << name << std::endl;
+    std::cout << "Dog constructor called for " << name << " at " << this << std::endl;
 }
 
 Dog::~Dog()
 {
-    delete p_age;
-    std::cout << "Dog destructor called for : " << name << std::endl;
+    delete p_age; // p_age is in Heap
+    std::cout << "Dog destructor called for " << name << " at " << this << std::endl;
 }
 
 int main()
 {
 
-    Dog dog1("Dogyy1", "Shepherd", 2);
-    Dog dog2("Dogyy2", "Shepherd", 3);
-    Dog dog3("Dogyy3", "Shepherd", 5);
-    Dog dog4("Dogyy4", "Shepherd", 1); // last object is destroyed first
+    Dog dog1("Fluffy", "Shepherd", 2); // Constructor
+    dog1.print_info();
 
+    /*
+    dog1.set_dog_name("Pumba");
+    dog1.set_dog_breed("Wire Fox Terrier");
+    dog1.set_dog_age(4);
+    */
+
+    // Chained calls using pointers, use arrow
+    // dog1.set_dog_name("Pumba")->set_dog_breed("Wire Fox Terrier")->set_dog_age(4);
+
+    // Chained calls using references, use dot
+    dog1.set_dog_name("Pumba").set_dog_breed("Wire Fox Terrier").set_dog_age(4);
+
+    dog1.print_info();
+
+    std::cout << "Done!" << std::endl;
+    // Destructor
     return 0;
 }
